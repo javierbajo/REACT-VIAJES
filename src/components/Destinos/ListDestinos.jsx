d8f2a8974e2165a93c51a023e2bf1abf9864e481
JaviBajo <javierbajobarrio@gmail.com>
2023-07-23 18:36:08 +0200

    Grupo destinos operativo

--- src/components/Destinos/ListDestinos.jsx
+++ src/components/Destinos/ListDestinos.jsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom';
 
 const renderDestinations = (destinations) =>{
     return destinations.map((destination,i) => (
-        <Link to={`./${destination.fruitName}`}style={{textDecoration: 'none'}} key={i}>
+        <Link to={`./${destination._id}`}style={{textDecoration: 'none'}} key={i}>
       <li key={destination.id} className="fruitCard">
         <div className="fruitCard-imageContainer">
           <img
-            src={destination.image}
-            alt={destination.fruitName}
+            src={destination.destinationImg[0]}
+            alt={destination.destinationPlace}
             className="fruitCard-image"
           />
         </div>
         <div className="fruitCard-info">
           <h2 className="fruitCard-name">
-            {destination.fruitName} ({destination.type})
+            {destination.destinationPlace} ({destination.destinationDescription})
           </h2>
-          <p className="fruitCard-p1">Origen: {destination.origin}</p>
-          <p className="fruitCard-p2">Precio:{destination.priceKg}€/Kg</p>
+          {/* <p className="fruitCard-p1">Origen: {destination.origin}</p>
+          <p className="fruitCard-p2">Precio:{destination.priceKg}€/Kg</p> */}
 
         </div>
       </li>
