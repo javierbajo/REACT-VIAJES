5998ae5a561fc17ba42237878f0f2c76cd55fc04
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2023-07-25 02:53:05 +0200

    componenteContactoScss

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,5 +1,5 @@
 import React, { useRef} from 'react';
-import '../../styles/Contacto.css';
+import '../../styles/Contacto/Contacto.css';
 import emailjs from '@emailjs/browser';
 
 
@@ -17,15 +17,27 @@ const Contacto = () => {
       });
   };
   return ( 
+   <><section className='container'>
+   <h2>Contacto</h2>
+   <h3>¿En qué podemos ayudarte?</h3>
+   <p>Si tienes cualquier duda sobre tu compra o consulta sobre nuestros servicios,envianos tu consulta y nosotros nos pondremos en contacto contigo.</p>
+   <h3>Puedes enviarnos tu consulta a través de este formulario</h3>
   <form ref={form} onSubmit={sendEmail} className='field'>
+  
     <label>Name</label>
-    <input type="text" name="user_name" />
+    <input type="text" name="user_name" placeholder='Escribe tu nombre'/>
     <label>Email</label>
-    <input type="email" name="user_email" />
+    <input type="email" name="user_email" placeholder='Escribe tu email'/>
     <label>Message</label>
-    <textarea name="message" />
+    <textarea name="message" placeholder='Escribe tu consulta'/>
+    <div className='checkbox'>
+  <input type="checkbox"/>Quiero que me enviéis novedades y ofertas de Viajes.net.
+</div>
     <input type="submit" value="Send" />
   </form>
+ 
+  </section>
+  </>
   );
 };
 
