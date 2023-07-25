4e24c7b7093a6ddbcde0d66f4d3dbbd435d31431
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2023-07-25 22:31:28 +0200

    componenteactividades

--- src/components/Actividades/ListActividades.jsx
+++ src/components/Actividades/ListActividades.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-// import '../../styles/ListFrutas.css'
+ import '../../styles/Actividades/Actividades.css';
 import { Link } from "react-router-dom";
 
 const renderActivities = (activities) => {
   return activities.map((activity, i) => (
     <Link to={`./${activity._id}`} style={{ textDecoration: "none" }} key={i}>
-      <li key={activity.id} className="fruitCard">
-        <div className="fruitCard-imageContainer">
+      <li key={activity.id} className="article_actividad">
+        <div className="actividad_imagenContainer">
           <img
             src={activity.activityImg}
             alt={activity.activityName}
-            className="fruitCard-image"
+            className="actividad_image"
           />
         </div>
-        <div className="fruitCard-info">
-          <h2 className="fruitCard-name">
+        <div className="actividad_info">
+          <h2 className="actividad_name">
             {activity.activityName} ({activity.activityDescription})
           </h2>
-          {/* <p className="fruitCard-p1">Origen: {destination.origin}</p>
-          <p className="fruitCard-p2">Precio:{destination.priceKg}€/Kg</p> */}
+           <p className="actividad_place">Place:{activity.activityPlace}</p>
+           <p className="actividad_date">Date:{activity.activityDate}</p>
+           <p className="actividad_time">Place:{activity.activityTime}</p>
+          <p className="actividad_precio">Precio:{activity.activityPrice}€/Kg</p>
         </div>
       </li>
     </Link>
   ));
 };
 const ListActividades = ({ activities }) => {
-  return <section>{renderActivities(activities)}</section>;
+  return <section className='section_actividades'>{renderActivities(activities)}</section>;
 };
 export default ListActividades;
